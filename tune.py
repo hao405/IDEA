416e8e1a9aacf8c60eabfbc3e7dae274c6af7659
zhu hao <1815526897@qq.com>
2025-10-23 00:59:12 +0800

    a

--- tune.py
+++ tune.py
@@ -128,7 +128,7 @@ def objective(trial):
     # 设置optuna超参
     args.learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-4, log=True)
     if args.data_path in ['electricity.csv','weather.csv']:
-        args.batch_size = trial.suggest_categorical('batch_size', [8,16,24])
+        args.batch_size = trial.suggest_categorical('batch_size', [16,24,32])
     else:
         args.batch_size = trial.suggest_categorical('batch_size', [16, 32, 48, 64])
     # args.dropout = trial.suggest_float('dropout', 0.05, 0.3)
