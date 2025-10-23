87e087470066ffbb1d287a6271465fbb4b26c763
zhu hao <1815526897@qq.com>
2025-10-23 16:36:22 +0800

    a

--- loop_tr.py
+++ loop_tr.py
@@ -7,19 +7,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-size', type=int, default=1)
 parser.add_argument('-dataset', type=str, nargs='+')
 parser.add_argument('-device', default=0, type=int)
+parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=True)
 # parser.add_argument('-lens', default=0, type=int, nargs='+')
 
 args = parser.parse_args()
 datalist = args.dataset
-pred_len_list = [24,36,48,60]
-learning_rates_list = [0.00001,0.0001,0.0005,0.001]
-batch_size_list = [8,16,32]
+pred_len_list = [96,192,336,720]
+learning_rates_list = [0.00001,0.00005,0.0001]
+batch_size_list = [16,24]
 comand_list = []
 # 新增列表用于记录每次循环的信息
 info_list = []
 seed_list = [2023]
 file_name = "all_IDEA"
-models_list = ["NSTS","NSTS1","NSTS2","NSTS3"]
+models_list = ["NSTS"]
 
 for data in datalist:
     for model in models_list:
@@ -27,7 +28,7 @@ for data in datalist:
             for lr in learning_rates_list:
                 for seed in seed_list:
                     for batch_size in batch_size_list:
-                        seq_len = 104
+                        seq_len = 96
                         label_len = 0
                         command = f"""     {d[data][str(pred_len)].replace("run.py", "run_nsts.py")}  --model {model} --seq_len {seq_len}  --is_bn --label_len 0  --pred_len {pred_len} --gpu {args.device}    --batch_size {batch_size}    --learning_rate {lr}     --train_epochs 40  --patience 5 --seed {seed} """
                         comand_list.append(command)
